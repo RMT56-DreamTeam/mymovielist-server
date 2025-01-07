df73bdec304d06f15055bea44851a099620bccba
khanz1 <assistance.xavier@gmail.com>
2025-01-07 13:49:21 +0700

    feat: login - add helpers jwt - add unique constraint in migrations - add model user - add POST /login endpoint and handlers

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const { User } = require("./models");
+const { comparePassword } = require("./helpers/bcrypt");
+const { generateToken } = require("./helpers/jwt");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isValidPassword = comparePassword(password, user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const access_token = generateToken({ id: user.id, email: user.email });
+    res.status(200).json({ message: "Login success", access_token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.listen(3000, () => {
--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../helpers/bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,12 +12,57 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: {
+          args: true,
+          msg: "Email already exists",
+        },
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Invalid email format",
+          },
+          notEmpty: {
+            args: true,
+            msg: "Email is required",
+          },
+          notNull: {
+            args: true,
+            msg: "Email is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "Password is required",
+          },
+          notNull: {
+            args: true,
+            msg: "Password is required",
+          },
+          len: {
+            args: [6],
+            msg: "Password minimum 6 characters",
+          },
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: (user, options) => {
+          user.password = hashPassword(user.password);
+        },
+      },
+      sequelize,
+      modelName: "User",
+    }
+  );
+
   return User;
 };
